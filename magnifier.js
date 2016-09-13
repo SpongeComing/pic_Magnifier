1ac25559f1b67048413951385600147eb3cf79cf
SpongeComing <923705388@qq.com>
2016-09-13 14:19:00 +0800

    change2 magnifier.js

--- magnifier.js
+++ magnifier.js
@@ -6,4 +6,11 @@ function magnifier (x,y) {
   oP.style.width = dWidth * scale + 'px';
   oP.style.height = dHeight * scale+'px';
 
+   var iWidth = parseInt(getStyle( oP,'width'));
+  var iHeight = parseInt(getStyle( oP,'height'));//放大镜宽高
+
+  if (x < iWidth/2){
+    a
+  }
+
 }
